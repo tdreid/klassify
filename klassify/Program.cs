b5ca0a251b6c712abdff755e51934a3aacba78ef
Trevor Reid <treid@bostonvalley.com>
2019-09-20 12:35:32 -0400

    Define output folder argument with default value

--- klassify/Program.cs
+++ klassify/Program.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utility.CommandLine;
 
 namespace klassify
 {
     class Program
     {
+        [Argument('o', "out", "output directory")]
+        private static string OutputDirectory { get; set; }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Arguments.Populate();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            if (String.IsNullOrEmpty(OutputDirectory))
+            {
+                OutputDirectory = ".";
+            }
+
+            Console.WriteLine("Output Dir: " + OutputDirectory);
+            Console.ReadKey();
         }
     }
 }
