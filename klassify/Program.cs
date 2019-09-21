3a6d00a2590e2c8fce187dfc1660cc0032c464f9
Trevor Reid <treid@bostonvalley.com>
2019-09-21 07:57:51 -0400

    Define additional command line arguments

--- klassify/Program.cs
+++ klassify/Program.cs
@@ -11,6 +11,21 @@ namespace klassify
         [Argument('o', "out", "output directory")]
         private static string OutputDirectory { get; set; }
 
+        [Argument('u', "user", "sql server user id")]
+        private static string UserId { get; set; }
+
+        [Argument('p', "password", "sql server password")]
+        private static string Password { get; set; }
+
+        [Argument('s', "server", "sql server")]
+        private static string Server { get; set; }
+
+        [Argument('d', "database", "sql database")]
+        private static string Database { get; set; }
+
+        [Argument('t', "timeout", "connection timeout")]
+        private static string Timeout { get; set; }
+
         static void Main(string[] args)
         {
             Arguments.Populate();
