86982346cadeb1f914090a3f7c7f910fcfcf13c7
Trevor Reid <treid@bostonvalley.com>
2019-09-21 07:54:57 -0400

    Organize usings

--- klassify/Program.cs
+++ klassify/Program.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using Utility.CommandLine;
 
 namespace klassify
